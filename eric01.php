e40b5e2ab3494f26119fc184812f33d272374bbd
Eric <eric2asd@gmail.com>
2017-05-17 16:37:11 +0800

    20170517v2

--- eric01.php
+++ eric01.php
@@ -5,7 +5,8 @@
  * Date: 2017/5/17
  * Time: 下午 03:56
  */
-echo 'Hello World!';
+echo 'Hello World!<br>';
+echo 'HI';
 
 ?>
 <h1>eric</h1>
---
+++ eric02.php
@@ -0,0 +1,13 @@
+<?php
+$var1 = 10;
+$var2 = 3 ;
+$var3 = $var1/ $var2;
+
+echo $var3;
+/*
+ * echo 'gettype(var): ';
+ * echo gettype($s153) ;
+*echo '<br>var_dump(var): ';
+*var_dump($var1);
+*/
+
