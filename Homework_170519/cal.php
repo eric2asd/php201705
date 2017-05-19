a548453a84db9f437750801dc82f20aec1714d4e
Eric <eric2asd@gmail.com>
2017-05-19 16:52:30 +0800

    20170519v2

--- Homework_170519/cal.php
+++ Homework_170519/cal.php
@@ -1,9 +1,22 @@
 <?php
-if(isset($_GET["x"])){
-$x = $_GET["x"];
-$y = $_GET["y"];
-$r= $x + $y ;
-//echo "$x + $y= $r";
+if (isset($_GET["x"])) {
+    $x = $_GET["x"];
+    $y = $_GET["y"];
+    $cal = $_GET["cal"];
+    $r = 0;
+    $a = 0;
+    if($cal==0){
+        $r = $x + $y;
+    }else if($cal==1){
+        $r = $x - $y;
+    }else if($cal==2){
+        $r = $x * $y;
+    }else if($cal==3){
+        $r = $x / $y;
+        $a = $x % $y;
+    }
+
+//
 }
 
 ?>
@@ -11,18 +24,22 @@ $r= $x + $y ;
 <!--<form action="eric04.php">-->
 <form>
     <input type="text" name="x" id="x" value="<?php echo $x ?>"/>
-    <select>
-        <option name="+" value="+">+</option>
-        <option name="-" value="-">-</option>
-        <option name="*" value="*">x</option>
-        <option name="/" value="/">/</option>
+    <select name="cal">
+        <option value="0">+</option>
+        <option value="1">-</option>
+        <option value="2">x</option>
+        <option value="3">/</option>
     </select>
     <input type="text" name="y" id="y" value="<?php echo $y ?>"/>
-<!--    <input type="button" value="=" onclick="cal()">-->
-<!--    <span id="result"></span>-->
-    <input type="submit" value="=" >
+    <!--    <input type="button" value="=" onclick="cal()">-->
+    <!--    <span id="result"></span>-->
+    <input type="submit" value="=">
     <?php
+    if($cal==3)
+        echo floor($r).'......'.$a;
+    else
         echo $r;
+
     ?>
 </form>
 
