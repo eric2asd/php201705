3fe0d6532737c8a3752213a0baa98390417b93a8
Eric <eric2asd@gmail.com>
2017-05-23 23:58:49 +0800

    homework complete

--- Homework/170523/Prime_number.php
+++ Homework/170523/Prime_number.php
@@ -1,29 +1,45 @@
-
-<table width="100%" border="1" >
+<table width="100%" border="1">
     <tr>
         <?php
-            for ($i=1;$i<=100;$i++){
-                $n[$i]=$i;
-            }
-
-            for($i=1;$i<=100;$i++){
-                if($i%10==1){
-                    echo '<tr>';
-                }
-                echo "<td>{$n[$i]}</td>";
-                if($i%10==0){
-                    echo '</tr>';
-                }
-            }
-
-
-        function prime_number(int $num){
-                $prime=false;
-                if($num==2||$num==3){
-                    $prime=true;
-                }else if ()
+        $total = 100;
+        for ($i = 1; $i <= $total; $i++) {
+            $n[$i] = $i;
         }
-            ?>
+
+        for ($i = 1; $i <= $total; $i++) {
+            if ($i % 10 == 1) {
+                echo '<tr>';
+            }
+            if (prime_number($i))
+                echo "<td style='background-color: yellow'>{$n[$i]}</td>";
+            else
+                echo "<td>{$n[$i]}</td>";
+            if ($i % 10 == 0) {
+                echo '</tr>';
+            }
+        }
+
+
+        function prime_number(int $num)
+        {
+            $prime = false;
+            if ($num == 2 || $num == 3) {
+                $prime = true;
+            } elseif ($num % 2 == 0) {
+
+            } else {
+                for ($i = 3; $i <= (int)($num / 2); $i += 2) {
+                    if ($num % $i == 0) {
+                        $prime = false;
+                        break;
+                    }
+                    $prime = true;
+                }
+            }
+            return $prime;
+        }
+
+        ?>
 
 
     </tr>
